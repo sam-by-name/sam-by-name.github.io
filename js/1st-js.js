36648342525cbcbf98537cf700c309712d83332b
sam-by-name <sam-sanders@hotmail.co.uk>
2018-04-07 01:01:42 +1200

    triangle madness

--- js/1st-js.js
+++ js/1st-js.js
@@ -1,16 +1,31 @@
-var user = prompt("enter a number between 0-10") ;
-if (user == 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)  {
-    var size = user;
-    var chess = "";
-    for (let i = 0; i < size; i++)  {
-      for (let x = 0; x < size; x++)  {
-        if ((i + x) % 2 == 0) {
-          chess += " ";
-        } else {
-          chess += "#";
+function myChess()  {
+  var x = document.getElementById("chessBtn").value;
+  document.getElementById("chessBtn").innerHTML = x;  {
+    var user = prompt("enter a number between 0-20") ;
+    if (user == 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)  {
+        var size = user;
+        var chess = "";
+        for (let i = 0; i < size; i++)  {
+          for (let x = 0; x < size; x++)  {
+            if ((i + x) % 2 == 0) {
+              chess += " ";
+            } else {
+              chess += "#";
+            }
+          }
+          chess += "\n";
         }
-      }
-      chess += "\n";
+    alert(chess);
     }
-alert(chess);
+  }
+}
+
+function myPyramid()  {
+    var x = document.getElementById("pyramid").value;
+    let triangle = "";                             
+    for (let i = 0; i < x; i++) {
+      alert(triangle = triangle + "#");                
+    document.getElementById("pyramid1").innerHTML= (triangle = triangle + "#")             
+  
+  }
 }
