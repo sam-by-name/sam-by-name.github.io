e5f5487401c330251ecf66160732f8d6beca1580
sam-by-name <sam-sanders@hotmail.co.uk>
2018-04-07 23:31:59 +1200

    gradients and stuff

--- js/1st-js.js
+++ js/1st-js.js
@@ -3,18 +3,18 @@ function myChess()  {
   document.getElementById("chessBtn").innerHTML = x;  {
     var user = prompt("enter a number between 0-20") ;
     if (user == 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)  {
-        var size = user;
-        var chess = "";
-        for (let i = 0; i < size; i++)  {
-          for (let x = 0; x < size; x++)  {
-            if ((i + x) % 2 == 0) {
-              chess += " ";
-            } else {
-              chess += "#";
-            }
+      var size = user;
+      var chess = "";
+      for (let i = 0; i < size; i++)  {
+        for (let x = 0; x < size; x++)  {
+          if ((i + x) % 2 == 0) {
+            chess += " ";
+          } else {
+            chess += "#";
           }
-          chess += "\n";
         }
+        chess += "\n";
+      }
     alert(chess);
     }
   }
@@ -22,10 +22,11 @@ function myChess()  {
 
 function myPyramid()  {
     var x = document.getElementById("pyramid").value;
-    let triangle = "";                             
-    for (let i = 0; i < x; i++) {
-      alert(triangle = triangle + "#");                
-    document.getElementById("pyramid1").innerHTML= (triangle = triangle + "#")             
-  
-  }
-}
+    document.getElementById("pyramid1").innerHTML = triangle;
+      var triangle = x;                             
+      for (let i = 0; i < x; i++) {
+        (triangle += "#") + (+ "\n");
+      }
+    alert(triangle);
+    }
+                     
