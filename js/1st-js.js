3b1d3f56328db4ae32fdf2562c36e75ef0c539b0
sam-by-name <sam-sanders@hotmail.co.uk>
2018-04-29 11:52:51 +1200

    triangle working

--- js/1st-js.js
+++ js/1st-js.js
@@ -1,33 +1,31 @@
 function myChess()  {
   var x = document.getElementById("chessBtn").value;
-  document.getElementById("chessBtn").innerHTML = x;  {
-    var user = prompt("enter a number between 0-20") ;
-    if (user == 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)  {
-      var size = user;
-      var chess = "";
-      for (let i = 0; i < size; i++)  {
-        for (let x = 0; x < size; x++)  {
-          if ((i + x) % 2 == 0) {
-            chess += " ";
-          } else {
-            chess += "#";
-          }
+  var user = prompt("enter a number between 0-20") ;
+  if (user == 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)  {
+    var size = user;
+    var chess = "";
+    for (let i = 0; i < size; i++)  {
+      for (let x = 0; x < size; x++)  {
+        if ((i + x) % 2 == 0) {
+          chess += " ";
+        } else {
+          chess += "#";
         }
-        chess += "\n";
       }
-    alert(chess);
+      chess += "\n";
     }
+  alert(chess);
   }
 }
-
-function myPyramid()  {
-  var x = document.getElementById("pyramid").value;
-  document.getElementById("pyramid1").innerHTML = triangle;{
-    var triangle = x;                             
-    for (let i = 0; i < x; i++) {
-      (triangle += "#") +  "\n";
-    }
-  alert(triangle);
+                  
+function myPyramid(x) {
+  x = document.getElementById("pyramid").value;
+  var result = "";                                 
+  for (var i = 0; i < x; i++) {
+    for (var j = 0; j < i; j++) {
+      result += "# ";
+    }                      
+    result += "\n";               
   }
+  alert(result);
 }
-                     
